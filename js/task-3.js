b1e108c185d0cd7f3f63424dd8c5022c34cf6435
Vasyl444 <152529131+Vasyl444@users.noreply.github.com>
2024-02-22 21:50:50 +0100

    HW-JS-01corrected

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,9 @@
 "use strict";
 function getElementWidth(content, padding, border) {
-    const getElementWidth = Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
+    const getElementWidth = parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
     console.log(getElementWidth);
     return getElementWidth;
 }
 getElementWidth("50px", "8px", "4px");
 getElementWidth("60px", "12px", "8.5px");
 getElementWidth("200px", "0px", "0px");
-
