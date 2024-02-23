808d88f6cf066bbddfcdf2f81dd774f036ad140b
Vasyl444 <152529131+Vasyl444@users.noreply.github.com>
2024-02-23 16:39:43 +0100

    HW-JS-01corrected1

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,8 @@
 "use strict";
 function getShippingMessage(country, price, deliveryFee) {
     const totalPrice = price + deliveryFee;
-    return console.log(`Shipping to ${country} will cost ${totalPrice} credits`);
+    return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
-getShippingMessage("Australia", 120, 50);
-getShippingMessage("Germany", 80, 20);
-getShippingMessage("Sweden", 100, 20);
+console.log(getShippingMessage("Australia", 120, 50));
+console.log(getShippingMessage("Germany", 80, 20));
+console.log(getShippingMessage("Sweden", 100, 20));
--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,8 @@
 "use strict";
 function getElementWidth(content, padding, border) {
-    const getElementWidth = Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
-    console.log(getElementWidth);
-    return getElementWidth;
+    const allElementsSum = Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
+    return allElementsSum;
 }
-getElementWidth("50px", "8px", "4px");
-getElementWidth("60px", "12px", "8.5px");
-getElementWidth("200px", "0px", "0px");
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
